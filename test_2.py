185bcd4477d10fae0840a9f62fc4d3665c866461
AlexanderLevkin <alexanlevkin@gmail.com>
2022-12-01 00:19:19 +0300

    Selenium_OOP

---
+++ test_2.py
@@ -0,0 +1,42 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Chrome
+
+from login_page import Login_page
+
+
+class Test_2():
+
+    def test_select_product(self):
+        driver = Chrome(executable_path=ChromeDriverManager().install())
+        base_url = 'https://www.saucedemo.com'
+        driver.get(base_url)
+        driver.maximize_window()
+        login_problem_user = 'problem_user'
+        password = 'secret_sauce'
+
+        print('Start Test')
+        login = Login_page(driver)
+        login.autorization(user_name=login_problem_user, login_password=password)
+
+        select_product = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.XPATH,'//button[@id="add-to-cart-sauce-labs-backpack"]')))
+        select_product.click()
+        print('Select product')
+
+        select_shoping_cart = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.XPATH,'//div[@id="shopping_cart_container"]')))
+        select_shoping_cart.click()
+        print('Click enter shopping cart')
+        time.sleep(2)
+
+        success_test = WebDriverWait(driver, 30).until(expected_conditions.element_to_be_clickable((By.XPATH,'//span[@class="title"]')))
+        success_test = success_test.text
+        print(success_test)
+        assert success_test == "YOUR CART"
+        print('Test success')
+
+
+test_2 =Test_2()
+test_2.test_select_product()
