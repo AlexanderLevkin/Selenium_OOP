8a13c9842c45bf5c5e3cfee8de9fad77cf353ba7
levkin.a <@AlexanderLevkin>
2022-12-15 15:21:21 +0300

    up-date

--- Task_4/NEW/test_2.py
+++ Task_4/NEW/test_2.py
@@ -8,12 +8,14 @@ user2 = USER(user_name=credentials()[2], login_password=credentials()[0])
 user3 = USER(user_name=credentials()[3], login_password=credentials()[0])
 user4 = USER(user_name=credentials()[4], login_password=credentials()[0])
 
-print(user1.getintosite())
+a = [user1, user2, user3, user4]
 
+while True:
+    for i in a:
+        try:
+            i.getintosite()
+        except NoSuchElementException as exception:
+            print("we catch this exception")
+            continue
+    break
 
-# for i in [user1.getintosite(),user2.getintosite(),user3.getintosite(),user4.getintosite()]:
-#     try:
-#         i
-#     except NoSuchElementException as exception:
-#         print("we catch this exception")
-#         continue
