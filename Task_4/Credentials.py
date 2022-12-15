55a7bba4ccb52f24983f5325ae28d4cf5796408d
levkin.a <@AlexanderLevkin>
2022-12-15 15:22:52 +0300

    up-date

--- Task_4/Credentials.py
+++ Task_4/Credentials.py
@@ -15,7 +15,6 @@ def credentials():
 #User_4 [4]
     login_problem_user_4 = 'performance_glitch_user'
 
-
     return password, login_problem_user_1, login_problem_user_2, login_problem_user_3, login_problem_user_4
 
 
